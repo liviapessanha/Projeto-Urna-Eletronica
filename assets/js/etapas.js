a92b0c60d07370b5230c09e92117ccfd8acb7ac1
Livia Pessanha <liviapessanha99@gmail.com>
2023-10-12 21:01:56 -0300

    inicial commit

---
+++ assets/js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '38111',
+        nome: 'Fulaninho',
+        partido: 'ABC',
+        fotos: [
+          {url: 'coracoralina.jpg', legenda: 'Vereador'}
+        ]
+      },
+      {
+        numero: '77222',
+        nome: 'Beltrano da Silva',
+        partido: 'DEFG',
+        fotos: [
+          {url: 'anisio-teixeira.jpg', legenda: 'Vereador'}
+       ]
+        },
+    ]
+  },
+{
+  titulo: 'PREFEITO',
+  numeros: 2,
+  candidatos: [
+    {
+      numero: '99',
+      nome: 'Ciclano',
+      partido: 'ABC',
+      vice: 'Nil',
+      fotos: [
+        {url: 'anisio-teixeira.jpg', legenda: 'Prefeito'},
+        {url: 'coracoralina.jpg', legenda: 'Vice-Prefeito'}
+      ]
+    },
+    {
+      numero: '84',
+      nome: 'Zulano',
+      partido: 'QWERT',
+      vice: 'Zul',
+      fotos: [
+        {url: 'anisio-teixeira.jpg', legenda: 'Prefeito'},
+        {url: 'coracoralina.jpg', legenda: 'Vice-Prefeito'}
+      ]
+    },
+  ]
+}
+];
